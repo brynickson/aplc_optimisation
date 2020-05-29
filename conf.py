0827534f011a8fd0e7a53d63e31098a5bbb50727
brynickson <bnickson@stsci.edu>
2020-05-29 13:10:01 -0400

    Add sphinx_rtd_theme and website logo

--- conf.py
+++ conf.py
@@ -15,6 +15,8 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -22,13 +24,12 @@ project = 'aplc-optimization'
 copyright = '2020, Bryony Nickson'
 author = 'Bryony Nickson'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,7 +51,8 @@ source_suffix = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_favicon = 'logo.jpg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
