3c38b08ccd176719e7de2cd9f3f89fe4d7cab1f8
brynickson <bnickson@stsci.edu>
2020-05-29 13:00:18 -0400

    Adding html build

--- conf.py
+++ conf.py
@@ -12,9 +12,9 @@
 #
 # import os
 # import sys
-
-sys.path.insert(0, os.path.abspath('.'))
 import os, sys
+sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
 
